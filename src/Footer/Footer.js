11cb62a7eb69b58280e41f369b10eee76f89b90a
Lucas Vocos <lucas.vocos@me.com>
2019-10-17 13:57:27 -0400

    add footer

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className='footer-navigation'>
         <h2 className='footer-title'>Lucas Vocos</h2>
         <nav className='footer-links'>
-          <a href={'tel:12134228271'} target="_blank" rel="noopener noreferrer">+1 213 422 8271</a>
+          <a href={'tel:12134228271'}>+1 213 422 8271</a>
           <a href={'mailto:hello@lucasvocos.com'} target="_blank" rel="noopener noreferrer">hello@lucasvocos.com</a>
           <a href={'https://www.instagram.com/lucasvocos'} target="_blank" rel="noopener noreferrer">Instagram</a>
         </nav>
