9581a8d00691eddce4aa5c6134e12c7d3443276d
Lucas Vocos <lucas.vocos@me.com>
2020-01-23 09:31:26 -0500

    restyle contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react'
+import MediaQuery from 'react-responsive'
+import { Link } from 'react-router-dom'
 import './Contact.scss'
 import Image from './lucas.JPG'
 
@@ -8,59 +10,96 @@ const Contact = () => {
   }, [])
 
   return (
-<main className='contact' aria-label="Contact Page">
-        <section className='contact-details'>
-          <aside className='contact-links'>
-            <p><a href={`tel:12134228271`}>+1 (213) 422 8271</a></p>
-            <p><a href={`mailto:hello@lucasvocos.com`}>hello@lucasvocos.com</a> </p>
-            <p><a href="https://github.com/lucasvocos" target="_blank" rel="noopener noreferrer">https://github.com/lucasvocos</a></p>
-            <p><a href="https://www.linkedin.com/in/lucasvocos/" target="_blank" rel="noopener noreferrer">https://www.linkedin.com/in/lucasvocos/</a></p>
-            <p><a href="https://www.instagram.com/lucasvocos/" target="_blank" rel="noopener noreferrer">https://www.instagram.com/lucasvocos/</a></p>
+    <main className='contact-page' aria-label="Contact Page">
+      <section className='contact-details'>
+        <MediaQuery maxWidth={1023}>
+          <aside className='bio'>
+            <h2 className='title'>About</h2>
+            <p>I'm a design-oriented full-stack web developer currently based in Detroit, MI. I specialize in developing elegant front-end experiences using React & Gatsby.</p>
+            <p>Please get in touch to collaborate on a project together</p>
           </aside>
-          <aside className='contact-img'>
-            <img src={Image} alt='Lucas Vocos' className='about-image-img'/>
+        </MediaQuery>
+        
+        <aside className='contact-links'>
+          <h2 className='title'>Contact</h2>
+          <ul className='links'>
+            <li><a href={`tel:12134228271`}>+1 (213) 422 8271</a></li>
+            <li><a href={`mailto:hello@lucasvocos.com`}>hello@lucasvocos.com</a> </li>
+            <li><a href="https://github.com/lucasvocos" target="_blank" rel="noopener noreferrer">Github</a></li>
+            <li><a href="https://www.linkedin.com/in/lucasvocos/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="https://www.instagram.com/lucasvocos/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+          </ul>
+        </aside>
+        <aside className='tech-stack'>
+          <h2 className='title'>Technology</h2>
+          <ul>
+            <li>Javascript</li>
+            <li>React.js</li>
+            <li>Gatsby.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>Sanity CMS</li>
+            <li>Contentful CMS</li>
+            <li>Sass</li>
+            <li>PostgreSQL Database</li>
+          </ul>
+        </aside>
+      </section>
+      <section className='contact-bio'>
+        <MediaQuery minWidth={1024}>
+          <aside className='bio'>
+            <p>I'm a design-oriented full-stack web developer currently based in Detroit, MI. I specialize in developing elegant front-end experiences using React & Gatsby.</p>
+            <p>Please get in touch to collaborate on a project together</p>
           </aside>
-          
-        </section>
-        <section className='resume'>
-          <div className='professional list'>
-            <h3>Professional</h3>
-            <ul className='work'>
-              <li><div className='year'>2018 - Present</div> Freelance Designer / Developer, Buena Suerte Studio</li>
-              <li><div className='year'>2013 - 2019</div> Director of Product Support, Brand Networks</li>
-              <li><div className='year'>2008 - 2013</div> Mac Genius, Apple</li>
-            </ul>
-          </div>
-          <div className='press list'>
-            <h3>Press</h3>
-            <ul className='work'>
-              <li><a href="https://www.fastcompany.com/90435036/this-site-generates-trump-notes-so-you-can-look-like-a-very-stable-genius-too" target="_blank" rel='noreferrer noopener'>Fast Company</a></li>
-              <li><a href='https://www.itsnicethat.com/news/quid-pro-sans-rick-banks-jkr-trump-typeface-graphic-design-261119' target='_blank' rel='noopener noreferrer'>It's Nice That</a></li>
-              <li><a href='https://lifehacker.com/write-a-note-in-trumps-handwriting-with-this-font-gener-1840035875' target='_blank' rel='noreferrer noopener'>Lifehacker</a></li>
-            </ul>
-          </div>
-          <div className='education list'>
-            <h3>Education</h3>
-            <ul className='ed'>
-              <li><div className='year'>2019</div> Full-Stack Engineering Immersion, Thinkful</li>
-            </ul>
-          </div>
-          <div className='technology'>
-            <h3>Tech Stack</h3>
-            <ul className='tech-stack'>
-              <li>Javascript</li>
-              <li>React.js</li>
-              <li>Gatsby.js</li>
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>Sanity CMS</li>
-              <li>Contentful CMS</li>
-              <li>Sass</li>
-              <li>PostgreSQL Database</li>
-            </ul>
-          </div>
-        </section>
-      </main>
+        </MediaQuery>
+        
+        <aside className='experience'>
+          <h2 className='title'>Experience</h2>
+          <ul className='work'>
+            <li>Currently — Freelance Designer / Developer, Buena Suerte Studio</li>
+            <li><div className='year'>2020</div> Design/development for Detroit-based photographer & artist  <Link to="work/james-adams"> James Adams.</Link></li>
+            <li><div className='year'>2019</div> Freelance design/development for <Link to="work/evelyn-crowley-portfolio"> Evelyn Crowley's Portfolio.</Link></li>
+            <li><div className='year'>2019</div> Development in collaboration with JKR on viral anti-Trump app <a href='https://www.finalwordfromthepres.com'>Final Word From the Pres</a> </li>
+            <li><div className='year'>2019</div> Design/development for Oakland-based DIY Punk label <a href='https://www.uncoolrecords.com'>Uncool Records</a> </li>
+            <li><div className='year'>2019</div> Freelance development for NY-based agency <a href='https://www.generalidea.agency'>General Idea</a> </li>
+            <li><div className='year'>2019</div> Design/development for Detroit-based boutique magazine shop <a href='https://www.bkst.co'>B_KS@</a> </li>
+            <li><div className='year'>2019</div> Development in collaboration with Hobbes on an interactive web app <a href='https://www.opal.work'>OP/AL</a> </li>
+            <li><div className='year'>2019</div> Design/development for <a href='https://www.articleoneeyewear.com'>Article One Eyewear</a> </li>
+            <li><div className='year'>2013 - 2019</div> Director of Product Support, Brand Networks</li>
+            <li><div className='year'>2008 - 2013</div> Mac Genius, Apple</li>
+          </ul>
+        </aside>
+        <aside className='clients'>
+          <h2 className='title'>Clients, Collaborators, & Friends</h2>
+          <ul className='clients-collabs'>
+            <li><a href="https://www.articleoneeyewear.com">Article One Eyewear</a></li>
+            <li><a href="https://www.hobbes.work">Hobbes</a></li>
+            <li><a href="https://www.instagram.com/washandfoldnyc">Wash & Fold NYC</a></li>
+            <li><a href="https://www.human.nyc">Human NYC</a></li>
+            <li><a href="https://www.jkrglobal.com/">Jones Knowles Ritchie</a></li>
+            <li><a href="https://www.uncoolrecords.com">Uncool Records</a></li>
+            <li><a href="https://www.monochrome.red">Monochrome.Red</a></li>
+          </ul>
+        </aside>
+      </section>
+      <section className='contact-press-ed'>
+        <aside className='press list'>
+          <h2 className='title'>Press</h2>
+          <ul className='work'>
+            <li><a href="https://www.fastcompany.com/90435036/this-site-generates-trump-notes-so-you-can-look-like-a-very-stable-genius-too" target="_blank" rel='noreferrer noopener'>Fast Company</a></li>
+            <li><a href='https://www.itsnicethat.com/news/quid-pro-sans-rick-banks-jkr-trump-typeface-graphic-design-261119' target='_blank' rel='noopener noreferrer'>It's Nice That</a></li>
+            <li><a href='https://lifehacker.com/write-a-note-in-trumps-handwriting-with-this-font-gener-1840035875' target='_blank' rel='noreferrer noopener'>Lifehacker</a></li>
+          </ul>
+        </aside>
+        <aside className='education list'>
+          <h2 className='title'>Education</h2>
+          <ul className='ed'>
+            <li><div className='year'>2019</div> Full-Stack Engineering Immersion, Thinkful</li>
+          </ul>
+        </aside>
+      </section>
+    </main>
+    
   )
 }
 
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,72 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import MediaQuery from 'react-responsive'
 import './Header.scss'
 
-export default class Header extends React.Component {
-  state = {
-    expanded: false
-  }
-  expandMenu = (e) => {
-    this.setState({
-      expanded: true
-    })
-  }
-  closeMenu = (e) => {
-    this.setState({
-      expanded: false
-    })
-  }
-
-  render() {
+const Header = () => {
     return (
       <header className="portfolio-header">
         <Link
           to={'/'}>
           <h1>Lucas Vocos</h1>
         </Link>
-        <MediaQuery maxWidth={1023}>
-          <button className='mobile-menu-trigger' onClick={this.expandMenu}>Menu</button>
-        </MediaQuery>
-        <MediaQuery minWidth={1024}>
-          <nav className='links'>
-            <Link
-              to={`/archive`}>
-              Archive
-            </Link>
-            <Link
-              to='/contact'>
-              Contact
-            </Link>
-          </nav>
-        </MediaQuery>
-        {this.state.expanded
-        ? (
-          <aside className='mobile-menu'>
-            <header className='mobile-menu-header'>
-              <Link
-                onClick={this.closeMenu}
-                to={'/'}>
-                <h1>Lucas Vocos</h1>
-              </Link>
-              <button className='mobile-menu-trigger' onClick={this.closeMenu}>Close</button>
-            </header>
-            <nav className='mobile-menu-links'>
-              <Link
-                onClick={this.closeMenu}
-                to={`/archive`}>
-                Archive
-              </Link>
-              <Link
-                onClick={this.closeMenu}
-                to='/contact'>
-                Contact
-              </Link>
-            </nav>
-          </aside>
-        )
-        : ''}
+        <Link
+          to='/contact'>
+          Info
+        </Link>
       </header>
     )
-  }
 }
+export default Header
+
